8fd7380e212bf5bd108d68ff1665cb6b9972c8c4
Zidan Bonanne Franco Rocha <zidanrocha@gmail.com>
2024-12-03 20:44:18 -0300

    quinto

--- pages/2_📊_Power_BI.py
+++ pages/2_📊_Power_BI.py
@@ -7,5 +7,5 @@ st.set_page_config(page_title="Power BI", page_icon="📊")
 st.markdown("# Power BI")
 
 
-components.iframe('"https://app.powerbi.com/view?r=eyJrIjoiNmE0NmYwOGYtZTQ1OS00ZDY1LThkNTctMGIxNTViMjFmMjI5IiwidCI6IjFjZWQ5ZDZjLWM2OTktNGMxYS1hYjlkLTI1YWY1MzI0ZGMyYSJ9',width=800,height=600,scrolling=False)
+components.iframe("https://app.powerbi.com/view?r=eyJrIjoiNmE0NmYwOGYtZTQ1OS00ZDY1LThkNTctMGIxNTViMjFmMjI5IiwidCI6IjFjZWQ5ZDZjLWM2OTktNGMxYS1hYjlkLTI1YWY1MzI0ZGMyYSJ9",width=800,height=600,scrolling=False)
 
